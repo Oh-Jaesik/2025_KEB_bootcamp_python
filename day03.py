e2f75a0991bad1a0957d60a86c146772416fdc26
Oh-jaesik <ojs72272@gmail.com>
2025-02-27 09:35:06 +0900

    v2.3

--- day03.py
+++ day03.py
@@ -1,38 +1,28 @@
 # Assignment
-# v2.7) Bug fix (아무거나 및 종료)
+# v2.3) 다음 코드에서 딕셔너리를 제거하고 리스트만 사용하여 동일하게 동작하도록 코드를 수정하시오.
 import random
-
-# d_s_p = {"위스키" : ['초콜릿', 50_000]}
-# print(d_s_p["위스키"][1])
-
 drinks = ["위스키", "와인", "소주", "고량주"]
 snacks = ["초콜릿", "치즈", "삽겹살", "양꼬치"]
-prices = [50000, 30000, 5000, 7500]
 
 drinks.append("사케")
 snacks.append("광어회")
-prices.append(25000)
 snacks[0] = "낙곱새"
-# drinks.append("데킬라")
-# snacks.append("소금")
-# prices.append(35000)
-
-def print_menu(n):
-    print(f'{drinks[n]}에 어울리는 안주는 {snacks[n]} 입니다')
-
-
-menu_list = '다음 술중에 고르세요.\n'
-for i in range(len(drinks)):
-    menu_list = menu_list + f'{i+1}) {drinks[i]}  '
-menu_list = menu_list + f'{len(drinks)+1}) 아무거나  {len(drinks)+2}) 종료 : '
 
 while True:
-    menu = int(input(menu_list))
-    if 1 <= menu <= len(drinks):
-        print_menu(menu - 1)
-    elif menu == len(drinks)+1:
+    menu = input(f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}   6) 아무거나   7) 종료 : ')
+    if menu == '1':
+        print(f'{drinks[0]}에 어울리는 안주는 {snacks[0]} 입니다')
+    elif menu == '2':
+        print(f'{drinks[1]}에 어울리는 안주는 {snacks[1]} 입니다')
+    elif menu == '3':
+        print(f'{drinks[2]}에 어울리는 안주는 {snacks[2]} 입니다')
+    elif menu == '4':
+        print(f'{drinks[3]}에 어울리는 안주는 {snacks[3]} 입니다')
+    elif menu == '5':
+        print(f'{drinks[4]}에 어울리는 안주는 {snacks[4]} 입니다')
+    elif menu == '6':
         random_index = random.randint(0, len(drinks)-1)
         print(f'{drinks[random_index]}에 어울리는 안주는 {snacks[random_index]} 입니다')
-    elif menu == len(drinks)+2:
+    elif menu == '7':
         print(f'다음에 또 오세요')
         break
